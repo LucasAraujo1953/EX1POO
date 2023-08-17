dc4bfff9963216c28e28a1495661c21bf14d2c44
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-08-17 19:31:48 -0300

    Add files via upload

---
+++ EX1POO/EX1POO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX1POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                Retangulo retangulo = new Retangulo();
+                retangulo.LerDados();
+                retangulo.ExibirArea();
+            }
+
+        }
+    }
+}
---
+++ EX1POO/EX1POO/Retangulo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX1POO
+{
+    internal class Retangulo
+    {
+        
+    private double baseRetangulo;
+        private double alturaRetangulo;
+
+        public void LerDados()
+        {
+            Console.Write("Digite a base do retângulo: ");
+            baseRetangulo = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a altura do retângulo: ");
+            alturaRetangulo = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public double CalcularArea()
+        {
+            return baseRetangulo * alturaRetangulo;
+        }
+
+        public void ExibirArea()
+        {
+            Console.WriteLine($"A área do retângulo é: {CalcularArea()}");
+        }
+    }
+}
+
